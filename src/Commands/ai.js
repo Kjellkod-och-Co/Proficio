664e4c4a4741ec0c12b0cd84c5dba8e692054ba6
Max Carlquist <tenkaklet@gmail.com>
2023-02-10 16:01:53 +0100

    Added

--- src/Commands/ai.js
+++ src/Commands/ai.js
@@ -18,7 +18,7 @@ module.exports = {
         }),
     execute: async (interaction, client) => {
         const question = interaction.options._hoistedOptions[0].value;
-        let language = undefined;
+        let language = 'text';
         language =  interaction.options._hoistedOptions[1].value;
         console.log('the language', language);
         interaction.deferReply();
